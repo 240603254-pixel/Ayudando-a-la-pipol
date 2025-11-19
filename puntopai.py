4db1d2177e20c7c808c332e7c52dafebe2f4a7c2
quijueracisterna <240603254@cecytejalisco.edu.mx>
2025-11-19 11:46:04 -0600

    diarrea

---
+++ puntopai.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+#ruta que envia un nombre a una plantilla
+@app.route("/tienda")
+def tienda():
+    return render_template('tienda.html')
+
+@app.route("/contacto")
+def contacto():
+    return render_template('contacto.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+@app.route("/galeria")
+def galeria():
+    return render_template('galeria.html')
+
+@app.route("/nosotros")
+def nosotros():
+    return render_template('nosotros.html')
+
+@app.route("/entrada")
+def entrada():
+    return render_template('entrada.html')
